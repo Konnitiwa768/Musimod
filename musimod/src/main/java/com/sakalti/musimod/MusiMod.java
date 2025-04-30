70ddfee58576dd21be05f31ad8dfba999738fe0e
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-04-30 17:15:32 +0900

    Update MusiMod.java

--- musimod/src/main/java/com/sakalti/musimod/MusiMod.java
+++ musimod/src/main/java/com/sakalti/musimod/MusiMod.java
@@ -2,6 +2,7 @@ package com.sakalti.musimod;
 
 import com.sakalti.musimod.registry.MusiModEntities;
 import com.sakalti.musimod.entity.CentipedeEntity;
+import com.sakalti.musimod.registry.MusiModSpawnEggs;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -14,6 +15,7 @@ public class MusiMod implements ModInitializer {
     @Override
     public void onInitialize() {
         MusiModEntities.register();
+        MusiModSpawnEggs.register();
         FabricDefaultAttributeRegistry.register(MusiModEntities.CENTIPEDE, CentipedeEntity.createCentipedeAttributes());
     }
 }
